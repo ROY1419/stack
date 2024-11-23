3dff1465d6f51208254fae3aba90272d21959de4
Roy1419 <royshubham471@gmail.com>
2024-11-23 20:22:38 +0530

    create env and config appwrite and next server

---
+++ src/models/server/answer.collection.ts
@@ -0,0 +1,7 @@
+import { answerCollection, db } from "../name";
+
+
+export default async function createanswerCollection() {
+    //create collection 
+    await databases.createanswerCollection(db, answerCollection, )
+}
