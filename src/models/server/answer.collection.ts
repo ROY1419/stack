cf922521864926dbcecc64cf8d4e36f951f691d4
Roy1419 <royshubham471@gmail.com>
2024-11-24 12:15:37 +0530

    this commit for server collection and client config

--- src/models/server/answer.collection.ts
+++ src/models/server/answer.collection.ts
@@ -1,7 +1,24 @@
-import { answerCollection, db } from "../name";
+import { Permission } from "node-appwrite"
 
+import {db, answerCollection} from "../name"
+import {databases} from "./config"
 
-export default async function createanswerCollection() {
-    //create collection 
-    await databases.createanswerCollection(db, answerCollection, )
+export default async function createAnswerCollection() {
+    //create collection
+    await databases.createCollection(db, answerCollection, answerCollection, [
+        Permission.read("any"),
+        Permission.read("users"),
+        Permission.create("users"),
+        Permission.update("users"),
+        Permission.delete("users"),
+    ])
+    console.log("Answer collection is created");
+
+    // creating attributes and indexes
+    await Promise.all([
+        databases.createStringAttribute(db, answerCollection, "content", 1000, true),
+        databases.createStringAttribute(db, answerCollection, "authorId", 50, true),
+        databases.createStringAttribute(db, answerCollection, "questionId", 50, false),
+    ])
+    console.log("Answer attributes created");
 }
